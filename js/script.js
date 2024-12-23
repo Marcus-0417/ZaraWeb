e57a784ad85ad0bd1d1b9702e09cd8e2e7be9423
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-23 17:07:25 +0800

    update

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+$(function () {
+
+    /* 滑鼠變換影像 */
+    $('.hover-img').hover(
+        function () {
+            // 滑鼠移入時更換圖片
+            var hoverSrc = $(this).data('hover');
+            $(this).attr('data-default', $(this).attr('src')); // 儲存原始圖片
+            $(this).attr('src', hoverSrc);
+        },
+        function () {
+            // 滑鼠移出時還原圖片
+            var hoverSrc = $(this).attr('data-default');
+            $(this).attr('src', hoverSrc);
+        });
+
+
+    /* jQuery UI tabs */
+    $("#tabs").tabs();
+    
+});
